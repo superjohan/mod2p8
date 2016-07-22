2af462fcc462e0088956607e314c374bba1bb73e
Johan Halin <johan@halin.me>
2016-07-22 13:37:56 +0300

    Very basic mod validity check: check for the 'M.K.' bytes in the header.

--- mod2p8.py
+++ mod2p8.py
@@ -2,6 +2,19 @@
 
 
 import click
+import struct
+import sys
+
+
+def parse_mod(input_mod):
+
+    with open(input_mod, mode='rb') as file:
+        mod_bytes = file.read()
+
+    mk_header = struct.unpack('4s', mod_bytes[1080:1084])[0].decode('ascii')
+
+    if mk_header != 'M.K.':
+        sys.exit('not a valid mod file')
 
 
 @click.command()
@@ -9,8 +22,7 @@ import click
 @click.argument('output_p8')
 def convert(input_mod, output_p8):
 
-    print("hello there")
-
+    parse_mod(input_mod)
 
 if __name__ == "__main__":
 
